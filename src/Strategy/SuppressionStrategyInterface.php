a842c68ca07d75bbe707a6ac5c7cc4008507f3f7
Patryk Knysak <jonsh4nnow@gmail.com>
2016-05-08 00:20:44 +0200

    encoding fix

--- src/Strategy/SuppressionStrategyInterface.php
+++ src/Strategy/SuppressionStrategyInterface.php
@@ -1,1 +1,18 @@
-<?phpnamespace KnysakPatryk\MediaQuerySuppressor\Strategy;/** * Interface SuppressionStrategyInterface * @package KnysakPatryk\MediaQuerySuppressor\Strategy */interface SuppressionStrategyInterface{    /**     * Performs suppression strategry on given input.     *     * @param $input     * @return string     */    public function suppress($input);}
+<?php
+
+namespace KnysakPatryk\MediaQuerySuppressor\Strategy;
+
+/**
+ * Interface SuppressionStrategyInterface
+ * @package KnysakPatryk\MediaQuerySuppressor\Strategy
+ */
+interface SuppressionStrategyInterface
+{
+    /**
+     * Performs suppression strategry on given input.
+     *
+     * @param $input
+     * @return string
+     */
+    public function suppress($input);
+}
--- src/Suppressor.php
+++ src/Suppressor.php
@@ -1,1 +1,57 @@
-<?phpnamespace KnysakPatryk\MediaQuerySuppressor;use KnysakPatryk\MediaQuerySuppressor\Strategy\SuppressionStrategyInterface;/** * This class helps you to "suppress" media queries in your dynamic content. * * Class Suppressor * @package KnysakPatryk\MediaQuerySuppressor */class Suppressor{    /**     * Suppression strategy class.     *     * @var SuppressionStrategyInterface     */    private $suppressionStrategy;    /**     * Creates instance of Suppression class.     *     * @param SuppressionStrategyInterface $suppressionStrategy     */    public function __construct(SuppressionStrategyInterface $suppressionStrategy)    {        $this->suppressionStrategy = $suppressionStrategy;    }    /**     * Suppress media query in passed string.     *     * @param $input     * @return string     */    public function one($input)    {        return $this->suppressionStrategy->suppress($input);    }    /**     * Suppress media queries in passed array.     *     * @param $array     * @return array     */    public function many($array)    {        foreach ($array as $key => $input) {            $array[$key] = $this->one($input);        }        return $array;    }}
+<?php
+
+namespace KnysakPatryk\MediaQuerySuppressor;
+
+use KnysakPatryk\MediaQuerySuppressor\Strategy\SuppressionStrategyInterface;
+
+/**
+ * This class helps you to "suppress" media queries in your dynamic content.
+ *
+ * Class Suppressor
+ * @package KnysakPatryk\MediaQuerySuppressor
+ */
+class Suppressor
+{
+    /**
+     * Suppression strategy class.
+     *
+     * @var SuppressionStrategyInterface
+     */
+    private $suppressionStrategy;
+
+    /**
+     * Creates instance of Suppression class.
+     *
+     * @param SuppressionStrategyInterface $suppressionStrategy
+     */
+    public function __construct(SuppressionStrategyInterface $suppressionStrategy)
+    {
+        $this->suppressionStrategy = $suppressionStrategy;
+    }
+
+    /**
+     * Suppress media query in passed string.
+     *
+     * @param $input
+     * @return string
+     */
+    public function one($input)
+    {
+        return $this->suppressionStrategy->suppress($input);
+    }
+
+    /**
+     * Suppress media queries in passed array.
+     *
+     * @param $array
+     * @return array
+     */
+    public function many($array)
+    {
+        foreach ($array as $key => $input) {
+            $array[$key] = $this->one($input);
+        }
+
+        return $array;
+    }
+}
